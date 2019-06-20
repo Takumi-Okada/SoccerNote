71fa708facb520002291fc96fcce3b8e0bc9982c
岡田匠 <okadatakumi@mbp-3.local>
2019-06-20 10:39:56 +0900

    ノート一覧、作成、詳細、編集画面を実装

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,41 @@
 class NotesController < ApplicationController
   def index
+    @notes=Note.all
   end
 
   def new
+    @note=Note.new
   end
 
   def show
+    @note=Note.find(params[:id])
   end
 
   def edit
+    @note=Note.find(params[:id])
+  end
+
+  def create
+    @note=Note.new(note_params)
+    if @note.save
+      redirect_to root_path,notice: 'ノートを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @note=Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to root_path,notice: 'ノートを更新しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title,:play_date,:contents,:good,:bad,:goal)
   end
 end
